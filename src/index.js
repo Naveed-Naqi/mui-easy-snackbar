6b352f9f65546979c1499228f0aa5af4c8f56fbc
Naveed Naqi <nnaqi5534@gmail.com>
2020-07-25 14:56:30 -0400

    added proper functions to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,73 @@
 import React from 'react'
-import styles from './styles.module.css'
+import { useSnackbar } from 'material-ui-snackbar-provider'
+import { Snackbar, Button } from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const useMuiEasySnackbar = () => {
+  const snackbar = useSnackbar()
+  return React.useMemo(() => {
+    const showMessage = (type) => (
+      message,
+      action,
+      handleAction,
+      customParameters
+    ) => {
+      let realMessage = message
+
+      if (type === 'error') {
+        if (typeof message !== 'string') {
+          const err = message
+          realMessage = (err.response && err.response.data) || err.message
+        }
+      }
+
+      snackbar.showMessage(realMessage, action, handleAction, {
+        ...customParameters,
+        type
+      })
+    }
+    return {
+      ...snackbar,
+      showInfo: showMessage('info'),
+      showWarning: showMessage('warning'),
+      showError: showMessage('error'),
+      showSuccess: showMessage('success')
+    }
+  }, [snackbar])
+}
+
+export const withMuiEasySnackbar = (Component) => {
+  return (props) => {
+    const snackbar = useMuiEasySnackbar()
+    return <Component snackbar={snackbar} {...props} />
+  }
+}
+
+const Alert = (props) => {
+  return <MuiAlert elevation={6} variant='filled' {...props} />
+}
+
+export const MuiEasySnackbar = ({
+  message,
+  action,
+  ButtonProps,
+  SnackbarProps,
+  customParameters
+}) => {
+  return (
+    <Snackbar autoHideDuration={3000} {...SnackbarProps}>
+      <Alert
+        severity={customParameters?.type}
+        action={
+          action != null && (
+            <Button color='inherit' size='small' {...ButtonProps}>
+              {action}
+            </Button>
+          )
+        }
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  )
 }
